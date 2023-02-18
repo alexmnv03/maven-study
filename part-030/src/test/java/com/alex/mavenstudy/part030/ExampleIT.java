d635fbed1449dd9916b2551423105e535e7b8509
alex_man <alexmnsbs@gmail.com>
2023-02-18 11:00:40 +0300

    is ready

---
+++ part-030/src/test/java/com/alex/mavenstudy/part030/ExampleIT.java
@@ -0,0 +1,15 @@
+package com.alex.mavenstudy.part030;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleIT {
+
+    @Test
+    public void exampleTest() {
+        assertTrue(true);
+    }
+
+}
